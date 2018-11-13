806fb9138fa4a27856f14f58649635026e5664c3
Kevin Nogales <kevinno18@gmail.com>
2018-11-13 15:06:16 -0500

    HODL now working with teams. Also rejecting malicious players.

---
+++ .#HODL.sol
@@ -0,0 +1,1 @@
+kevinnogales@Kevins-MacBook-Pro.local.5641
--- HODL.sol
+++ HODL.sol
@@ -9,6 +9,7 @@ contract HODL {
 
 	struct Player {
 		Deposit userDeposit;
+		bool assignedToTeam;
 	}
 
 	struct Team {
@@ -16,10 +17,16 @@ contract HODL {
 		mapping(address => Player) players;
 	}
 
+	// Mapping will hold both teams. Team 1 and Team 2. 
 	mapping(int => Team) teams;
 
+	// This mapping will hold all the malicious players. 
+	// Their address will map to true if they are malicious.
+	mapping(address => bool) malicious;
+
 	uint8 public teamSize;
-	uint256 public RELEASE_TIME = 1 minutes;
+	// Time will be varied and updated. 
+	uint256 public RELEASE_TIME = .5 minutes;
 
 	// This will be set to false once the match is finished. 
 	bool matchIsFinished = false;
@@ -40,21 +47,38 @@ contract HODL {
 	function deposit(uint8 teamNumber) public payable {
 		// User must pick either 1 or 2 for the team number.
 		require(teamNumber == 1 || teamNumber == 2);
+
 		// The user must also submit a value that is greater than 0 
 		require(msg.value > 0);
+
+		// Match must still be in session. 
 		require(matchIsFinished == false);
 
 		// Make sure that the team the user is trying to enter is not filled. 
 		require(teams[teamNumber].playersCurrentSize < teamSize);
 
-		if (teams[teamNumber].players[msg.sender].userDeposit.releaseTime == 0) {
+		// User cannot be in the other team or in the current team.
+		// If the user is in the other team or in the current team,
+		// penalize them and label as malicious player. 
+
+		// If the user is already labelled a malivious player
+		// revert and do not do anything. 
+		if (malicious[msg.sender]) {
+			revert();
+		} else if (teams[1].players[msg.sender].assignedToTeam || teams[2].players[msg.sender].assignedToTeam) {
+			// If the user is already in a team, label the user malicious and revert. 
+			malicious[msg.sender] = true;
+			revert();
+		} else if (teams[teamNumber].players[msg.sender].userDeposit.releaseTime == 0) {
+			// Add the user to the specified team and set their release time. 
 			uint256 releaseTime = now + RELEASE_TIME;
 			teams[teamNumber].players[msg.sender].userDeposit = Deposit(msg.value, releaseTime);
 
+			// Set assignedToTeam flag to true. 
+			teams[teamNumber].players[msg.sender].assignedToTeam = true;
+
 			// Update the size of the team.
 			teams[teamNumber].playersCurrentSize = teams[teamNumber].playersCurrentSize + 1;
-		} else {
-			// We could add the amount they want to add. 
 		}
 	}
 
