cf60febf5ae3ae0a2b8f8375fb33979b697c4497
twburk <burkt95@gmail.com>
2018-11-27 18:53:20 -0500

    working on payWinner

--- HODL.sol
+++ HODL.sol
@@ -26,7 +26,8 @@ contract HODL {
 	uint8 public teamSize;
 
 	// Time will be varied and updated. 
-	uint256 public RELEASE_TIME = .5 minutes;
+	// Do not need anymore. Only for testing. 
+	// uint256 public RELEASE_TIME = .5 minutes;
 
 	// This will be set to true once all the teams are filled. 
 	bool matchIsLocked = false;
@@ -35,6 +36,10 @@ contract HODL {
 	bool matchIsFinished = false;
 
 	uint8 winningTeamIndex;
+
+	// These arrays will hold the addresses of the players in each team. 
+	address[] public teamOneAddresses;
+	address[] public teamTwoAddresses;
 
 	function HODL(uint8 size) public {
 		// The size of the teams must greater than 0 and less than 4. 
@@ -79,9 +84,9 @@ contract HODL {
 			revert();
 		} else if (teams[teamNumber].players[msg.sender].assignedToTeam == false) {
 			// Add the user to the specified team and set their release time. 
-			uint256 releaseTime = now + RELEASE_TIME;
+			// uint256 releaseTime = now + RELEASE_TIME;
 
-			teams[teamNumber].players[msg.sender].userDeposit = Deposit(msg.value, releaseTime);
+			teams[teamNumber].players[msg.sender].userDeposit = Deposit(msg.value);
 
 			// Set assignedToTeam flag to true. 
 			teams[teamNumber].players[msg.sender].assignedToTeam = true;
@@ -89,8 +94,16 @@ contract HODL {
 			// Update the size of the team.
 			teams[teamNumber].playersCurrentSize = teams[teamNumber].playersCurrentSize + 1;
 
-			///////////////////////////////////////////////////////////////////////
+			// ADD ADDRESS TO ARRAY
+			// Add to teamOneAddresses if teamNumber is 1
+			// Add to teamTwoAddresses if teamNumber is 2
+			if (teamNumber == 1) {
+				teamOneAddresses.push(msg.sender);
+			} else {
+				teamTwoAddresses.push(msg.sender);
+			}
 
+			///////////////////////////////////////////////////////////////////////
 
 			// Check if both teams are filled.
 			// If both teams are filled set flag to not allow any other users to join.
@@ -100,23 +113,23 @@ contract HODL {
 		}
 	}
 
-	function initiateWithdrawal() public {
+	/*function initiateWithdrawal() public {
 		// Check if matchIsFinished is true.
 		// If it is true, then do not continue function and call the revert() function.
 		// if it is false, then continue function and calc losses and pay winners and non malicious players. 
 		if(matchIsFinished == true){
 			revert();
-		}else if(matchIsFinished == false){
+		} else if(matchIsFinished == false){
 			uint8 sum = 0;
 
 			// Check what team the player that initiates withdrawal is in.
 			// Make the winningTeamIndex equal to the index of the opposite team.
 			// call the calculate loss function to calculate how money is going to be summed up to.
 			// Should be the sum of the penalties from the losing team and the penalties from the malicious players.
-			if(teams[1].players[msg.sender] == true){
+			if(teams[1].players[msg.sender].assignedToTeam == true){
 				winningTeamIndex = 2;
 				sum = calculateLoss(1);
-			}else if(teams[2].players[msg.sender] == true){
+			}else if(teams[2].players[msg.sender].assignedToTeam == true){
 				winningTeamIndex = 1;
 				sum = calculateLoss(2);
 			}
@@ -128,11 +141,10 @@ contract HODL {
 			// set the matchIsFinished vairable to true so that functions can no longer be called. 
 			matchIsFinished = true;
 		}
-		
-	}
+	}*/
 
 	// This function only with wirh releaseTime in the Deposite structure. 
-	function withdraw(uint8 teamNumber) public {
+	/*function withdraw(uint8 teamNumber) public {
 		require(teamNumber == 1 || teamNumber == 2);
 		require(teams[teamNumber].players[msg.sender].userDeposit.value > 0);
         require(teams[teamNumber].players[msg.sender].userDeposit.releaseTime < now);
@@ -141,12 +153,34 @@ contract HODL {
         
         teams[teamNumber].players[msg.sender].userDeposit.value = 0;
         teams[teamNumber].players[msg.sender].userDeposit.releaseTime = 0;
-	}
+	}*/
 
 	//sum of the penalties from the losing team and the penalties from the malicious players.
 	function calculateLoss(uint8 teamNumber) public {
 		uint8 sum = 0;
+		uint8 percentLoss;
 
+		if(teamNumber == 1){
+			uint arrayLength = teamOneAddresses.length;
+			address[] public tempArray = teamOneAddresses;
+		}else{
+			uint arrayLength = teamTwoAddresses.length;
+			address[] public tempArray = teamTwoAddresses;
+		}
+
+		for(uint i=0; i < arrayLength; i++){
+			uint8 playerValue = teams[teamNumber].players[tempArray[i]].userDeposit.value;
+			if(malicious[tempArray[i]]){
+				percentLoss = .40;
+			}else{
+				percentLoss = .15;
+			}
+
+			uint8 percentAmount = playerValue * percentLoss;
+			sum += percentAmount;
+			uint8 newPlayerValue = playerValue - percentAmount;
+			teams[teamNumber].players[tempArray[i]].userDeposit = newPlayerValue;
+		}
 
 
 		return sum;
@@ -154,6 +188,19 @@ contract HODL {
 
 	// Pay each winner their respective winning amount
 	function payWinner(uint8 winningTeam, uint8 winningSum) public{
+		if(winningTeam == 1){
+			uint arrayLength = teamOneAddresses.length;
+			address[] public tempArray = teamOneAddresses;
+		}else{
+			uint arrayLength = teamTwoAddresses.length;
+			address[] public tempArray = teamTwoAddresses;
+		}
+
+		unit total = winningSum / arrayLength;
+
+		for(uint i=0; i < arrayLength; i++){
+			msg.sender.transfer(teams[teamNumber].players[msg.sender].userDeposit.value);
+		}
 
 	}
 }
